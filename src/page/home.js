22b74446e8c3b931c382d131a5a745bb984b95fb
Aysgl <aysegul.avcu@hotmail.com>
2022-02-19 22:03:00 +0300

    Update home.js

--- src/page/home.js
+++ src/page/home.js
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <div className="d-flex vh-100 py-5">
             <Container className='d-flex my-auto flex-column'>
-                <Row className="g-5 mx-5">
+                <Row className="g-5 mx-md-5">
                     <Col lg={7} className="order-lg-1 order-2">
                         <Forms />
                     </Col>
